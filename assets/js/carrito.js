01efd1a1f0cc94bd21d74dbe16bdf9d86fb9bd4f
FreddyTurbina133 <die.sancheza@duocuc.cl>
2025-09-05 04:00:52 -0400

    Se agrego el carrito de compras en la seccion de Teclados, se modifico el diseño de ciertos titulos y se agrego de manera predeterminada a casi todos los archivos un html:5

---
+++ assets/js/carrito.js
@@ -0,0 +1,10 @@
+class Producto {
+    id;
+    nombre;
+    precio;
+    constructor(id, nombre, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }    
+}
