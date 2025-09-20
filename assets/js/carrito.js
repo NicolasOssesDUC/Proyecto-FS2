7d91bd0ebce94c823174093303739d203a936569
royel <rola.paredes@duocuc.cl>
2025-09-20 15:17:08 -0300

    cambios sobre carrito

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -8,3 +8,21 @@ class Producto {
         this.precio = precio;
     }    
 }
+
+// Inicializar carrito si no existe
+    if (!localStorage.getItem("carrito")) {
+        localStorage.setItem("carrito", JSON.stringify([]));
+    }
+
+function actualizarContador() {
+    // Obtener carrito de localStorage
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    // Actualizar el span del contador
+    let contadorElemento = document.getElementById("contador");
+    if (contadorElemento) {
+        contadorElemento.innerText = carrito.length;
+    }
+    console.log("Carrito actual:", localStorage.getItem("carrito"));
+}
+
+document.addEventListener("DOMContentLoaded", actualizarContador);
