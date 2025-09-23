412a34f3e9002e6dc023bc97940df4135208c21a
Nicolas <m.ossesnicolas@gmail.com>
2025-09-23 01:50:18 -0300

    si

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -6,19 +6,18 @@
     if (!usuarioLogueadoString) {
         console.log('Acceso denegado: No hay usuario en sesión.');
         window.location.href = '../login.html';
-        return; // Detener la ejecución
+        return; 
     }
 
-    // 2. Convertir el string a un objeto
+    
     const usuario = JSON.parse(usuarioLogueadoString);
 
-    // 3. Comprobar si el rol es 'Administrador'
+    // 2. Comprobar si el rol es 'Administrador'
     if (usuario.rol !== 'Administrador') {
         console.log(`Acceso denegado: El usuario ${usuario.nombre} no tiene rol de Administrador.`);
         window.location.href = '../index.html';
     }
 
-    // Si todo está bien, el script termina y la página de admin se carga normalmente.
     console.log(`Acceso permitido: Hola, ${usuario.nombre}.`);
 })();
 
